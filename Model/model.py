b4ddd8dff2705e09ac4e825f333e0bb1a459f0e1
Nicolas Sebag <206442662+nicolassebag@users.noreply.github.com>
2025-06-03 09:39:55 +0200

    Refactor model training and evaluation functions; update compile method to use sparse categorical crossentropy

--- Model/model.py
+++ Model/model.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 from typing import Tuple
 
+from colorama import Fore, Style
+
 # KERAS
-from keras import Sequential, Input, layers
+from keras import Model, Sequential, Input, layers
 from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.optimizers.schedules import ExponentialDecay
@@ -35,22 +37,78 @@ def compile_model(model: Model, learning_rate=0.0005) -> Model:
     Compile the Neural Network
     """
     optimizer = Adam(learning_rate=learning_rate)
-    model.compile(optimizer='adam',
-                  loss='categorical_crossentropy',
+
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
-    pass
+    print("✅ Model compiled")
+    return model
 
-def train(model,X,y):
-    batch_size = 16
-    epochs = 10
-    es = EarlyStopping(patience=2)
-    validation_split = 0.1
 
-    model.fit(X,
-          y,
-          batch_size=batch_size,
-          epochs=epochs,
-          callbacks=[es],
-          validation_split=validation_split
-          )
-    return model.get_metrics_result()
+def train_model(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=16,
+        patience=2,
+        validation_data=None, # overrides validation_split
+        validation_split=0.3
+    ) -> Tuple[Model, dict]:
+    """
+    Fit the model and return a tuple (fitted_model, history)
+    """
+    print(Fore.BLUE + "\nTraining model..." + Style.RESET_ALL)
+
+    es = EarlyStopping(
+        monitor="val_loss",
+        patience=patience,
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    history = model.fit(
+        X,
+        y,
+        validation_data=validation_data,
+        validation_split=validation_split,
+        epochs=100,
+        batch_size=batch_size,
+        callbacks=[es],
+        verbose=0
+    )
+
+    print(f"✅ Model trained on {len(X)} rows with min val MAE: {round(np.min(history.history['val_mae']), 2)}")
+
+    return model, history
+
+def evaluate_model(
+        model: Model,
+        X: np.ndarray,
+        y: np.ndarray,
+        batch_size=64
+    ) -> Tuple[Model, dict]:
+    """
+    Evaluate trained model performance on the dataset
+    """
+
+    print(Fore.BLUE + f"\nEvaluating model on {len(X)} rows..." + Style.RESET_ALL)
+
+    if model is None:
+        print(f"\n❌ No model to evaluate")
+        return None
+
+    metrics = model.evaluate(
+        x=X,
+        y=y,
+        batch_size=batch_size,
+        verbose=0,
+        # callbacks=None,
+        return_dict=True
+    )
+
+    loss = metrics["loss"]
+    accuracy = metrics["accuracy"]
+
+    print(f"✅ Model evaluated, MAE: {round(accuracy, 2)}")
+
+    return metrics
